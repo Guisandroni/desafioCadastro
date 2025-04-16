1f795ff47fe1906f5abfd25f51a0daa0c115dfad
guisandroni <gui.sandroni@gmail.com>
2025-04-16 13:12:10 -0300

    refactor: update .gitignore, improve API documentation, and enhance pet registration validation

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { deletePet, listPets,  registerPet,  updatePet } from "./services/pet.ts";
+import { deletePet, listPetByIdade, listPetByName, listPetByRaca, listPets,  registerPet,  updatePet } from "./services/pet.ts";
 
 export const appRoutes = (app: FastifyInstance) => {
   app.post("/animals/pet/register", registerPet);
@@ -7,4 +7,7 @@ export const appRoutes = (app: FastifyInstance) => {
   app.put('/animals/pet/:id', updatePet)
   app.post('/animals/pet/update/:id', updatePet)
   app.get('/animals/pet', listPets)
+  app.get('/animals/pet/name/:nome', listPetByName) 
+  app.get('/animals/pet/idade/:idade', listPetByIdade)
+  app.get('/animals/pet/raca/:raca', listPetByRaca)
 };
--- src/routes/services/pet.ts
+++ src/routes/services/pet.ts
@@ -33,6 +33,15 @@ export const registerPet = async (
   });
   const { nome, sobrenome, idade, raca, sexo, endereco, peso } =
     paramsRegisterPet.parse(request.body);
+
+
+if(peso<=0.5 && peso>=60){
+  return reply.status(501).send({error:'Peso do animal fora do padrão'})
+}
+if(endereco.length<10){
+  return reply.status(501).send({error:'Endereço invalido'})
+}
+
   await prisma.animals.create({
     data: {
       nome,
@@ -103,3 +112,52 @@ export const updatePet = async (request:FastifyRequest, reply:FastifyReply) => {
 
   return reply.status(200).send({ message: "pet atualizado" });
 };
+
+
+export const listPetByName = async (request:FastifyRequest, reply:FastifyReply) => {
+  const paramsPetByName = z.object({
+    nome:z.string(),
+  })
+
+  const {nome} = paramsPetByName.parse(request.params)
+
+  const data = await prisma.animals.findMany({
+    where:{
+      nome:nome
+    }
+  })
+
+  return reply.status(200).send({data})
+}
+
+export const listPetByIdade = async (request:FastifyRequest, reply:FastifyReply) => {
+  const paramsPetByIdade = z.object({
+    idade:z.number(),
+  })
+
+  const {idade} = paramsPetByIdade.parse(request.params)
+
+  const data = await prisma.animals.findMany({
+    where:{
+      idade:idade
+      }
+  })
+
+  return reply.status(200).send({data})
+}
+
+export const listPetByRaca = async (request:FastifyRequest, reply:FastifyReply) => {
+  const paramsPetByRaca = z.object({
+    raca:z.string(),
+  })
+
+  const {raca} = paramsPetByRaca.parse(request.params)
+
+  const data = await prisma.animals.findMany({
+    where:{
+      raca:raca
+      }
+  })
+
+  return reply.status(200).send({data})
+}
